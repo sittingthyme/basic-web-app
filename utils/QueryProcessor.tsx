f29847e3b8dc3633b388fb1e2aeaac7466873a7f
sittingthyme <nicholan@andrew.cmu.edu>
2026-02-24 09:50:34 -0500

    fix

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "nicholan";
   }
   if (query.toLowerCase().includes("andrew id")) {
     return "nicholan";
