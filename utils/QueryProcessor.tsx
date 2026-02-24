26d4df373dc003d3115deaa94c05b6c882affca9
sittingthyme <nicholan@andrew.cmu.edu>
2026-02-24 10:20:24 -0500

    cube and square

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -44,5 +44,25 @@ export default function QueryProcessor(query: string): string {
       return String(left * right);
     }
   }
+  if (query.toLowerCase().includes("square and a cube")) {
+    const match = query.match(/square\s+and\s+a\s+cube:\s+(\d+)+,\s+(\d+)+,\s+(\d+)+,\s+(\d+)+,\s+(\d+)+,\s+(\d+)+,\s+(\d+)/i);
+    if (match) {
+      const num1 = parseInt(match[1], 10);
+      const num2 = parseInt(match[2], 10);
+      const num3 = parseInt(match[3], 10);
+      const num4 = parseInt(match[4], 10);
+      const num5 = parseInt(match[5], 10);
+      const num6 = parseInt(match[6], 10);
+      const num7 = parseInt(match[7], 10);
+      let nums: number[] = [num1, num2, num3, num4, num5, num6, num7];
+      let res: number[] = []
+      for (let num of nums) {
+        if (Math.sqrt(num) % 1 === 0 && Math.cbrt(num) % 1 === 0) {
+          res.push(num);
+        }
+      }
+      return String(res.join(", "));
+    }
+  }
   return "";
 }
