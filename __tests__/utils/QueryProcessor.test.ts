9e1012247a74db67d7203607457110fa71075d1d
sittingthyme <nicholan@andrew.cmu.edu>
2026-02-24 09:47:32 -0500

    fix

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -30,7 +30,7 @@ describe("QueryProcessor", () => {
         const query = "What is your andrew id?";
         const response: string = QueryProcessor(query);
         expect(response).toBe((
-            "Lebron James"
+            "nicholan"
           ));
     });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,7 +11,7 @@ export default function QueryProcessor(query: string): string {
     return "Rohan";
   }
   if (query.toLowerCase().includes("andrew id")) {
-    return "Lebron James";
+    return "nicholan";
   }
   return "";
 }
